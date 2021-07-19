7d4d8e5e47f198d34ca872002cf6888e3245bfff
Esteban <e.sepulveda.sep@gmail.com>
2021-07-18 22:48:41 -0400

    viajes chile version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,32 @@ $(function(){
       });
 
     }
-
-
-
+  })
+      //scroll
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= $(header).height() ) {
+         $('nav').addClass('cabeceraOscura');
+      } else{
+         $('nav').removeClass('cabeceraOscura');
+      }
   });
 
 
-})
+          //toogle
+  $( "#clickme1" ).click(function() {
+    $( "#cuerpo1" ).toggle( "800", function() {
+    });
+    });
+  $( "#clickme2" ).click(function() {
+    $( "#cuerpo2" ).toggle( "800", function() {
+    });
+    });
+  $( "#clickme3" ).click(function() {
+    $( "#cuerpo3" ).toggle( "800", function() {
+    });
+    });
+      //tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
